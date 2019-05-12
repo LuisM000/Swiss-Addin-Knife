bc8c97cd9bd5ec76de7b854b3a860fe849c5e528
LuisM000 <luis.m.r@outlook.com>
2019-05-12 20:48:36 +0200

    Project added

---
+++ SwissAddinKnife/Properties/AddinInfo.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "SwissAddinKnife",
+    Namespace = "SwissAddinKnife",
+    Version = "0.0.1"
+)]
+
+[assembly: AddinName("SwissAddinKnife")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("SwissAddinKnife")]
+[assembly: AddinAuthor("Luis Marcos Rivera")]
